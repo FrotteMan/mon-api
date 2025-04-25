10c1f8a6cfa3ef4cd1838cb63c80582e3450c48a
Julien <julienl.science@gmail.com>
2025-04-25 11:45:35 +0200

    Mise à jour

--- connexion.php
+++ connexion.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=172.29.103.63;dbname=ffbsq_competitions;charset=utf8', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=ffbsq_competitions;charset=utf8', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
