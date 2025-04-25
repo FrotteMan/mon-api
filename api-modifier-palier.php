bfd5ec23cd1f813ffee667214c5710d10a8dcd1f
Julien <julienl.science@gmail.com>
2025-04-25 14:25:54 +0200

    Mise à jour

---
+++ api-modifier-palier.php
@@ -0,0 +1,41 @@
+<?php
+require 'connexion.php';
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'GET') {
+	// Récupère le palier.
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        // Préparer la requête.
+        $stmt = $pdo->prepare("SELECT * FROM palier WHERE id_palier = ?");
+        $stmt->execute([$id]);
+        $palier = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo json_encode($palier);
+    }
+	// Vérification de l'ID
+	if (!isset($_GET['id'])) {
+		die("ID du palier manquant.");
+	}
+} elseif ($method === 'POST') {
+    // Mise à jour d'un palier
+    if (isset($_POST['id_palier'])) {
+        $id = $_POST['id_palier'];
+        $nom = $_POST['nom_palier'];
+        $nbr_pts_minimum = $_POST['nbr_pts_minimum_palier'];
+
+        // Préparer la requête UPDATE
+        $sql = $pdo->prepare("UPDATE palier SET nom_palier=?, nbr_pts_minimum_palier=? WHERE id_palier=?");
+        $sql->execute([$nom, $nbr_pts_minimum, $id]);
+
+        echo json_encode(['success' => 'Palier mise à jour']);
+    } else {
+        echo json_encode(['error' => 'ID de palier manquant']);
+    }
+else {
+    http_response_code(405); // Méthode non autorisée
+    echo json_encode(['error' => 'Méthode non autorisée']);
+}
